9946d3e7c25f22aef60ada4c491a45fa8c86cca5
Claudia Andrade <andrade8a@gmail.com>
2015-05-15 13:19:47 -0700

    Avance 1 angular

--- Flatlanders/Content/js/app.js
+++ Flatlanders/Content/js/app.js
@@ -1,14 +1,37 @@
 ﻿(function () {
     var app = angular.module('store', []);
     app.controller('StoreController', function () {
-        this.product = gem;
+        this.products = gems;
+    });
+    
+    app.controller('PanelController', function () {
+        this.tab = 1;
+        this.selectedTab = function (setTab) {
+            this.tab = setTab;
+        };
+        this.isSelected = function(checkTab){
+            return this.tab === checkTab;
+        };
     });
 
-    var gem = {
-        name: 'Dodecahedron',
-        price: 2.95,
-        description: '. . .',
-        canPurchase: false,
-        soldOut: true,
-    };
+    var gems = [
+        {
+            name: 'Dodecahedron',
+            price: 2.95,
+            description: 'this is our best gem because has a lot  of colors and properties thar cure the cancer',
+            canPurchase: true,
+            images: [
+                {
+                    full:'/Content/img/gem-02.jpg',
+                    thumb:'/Content/img/gem-02.jpg',
+                }
+            ]
+        },
+        {
+            name: 'Pentagonal Gem',
+            price: 5.95,
+            description: '. . .',
+            canPurchase: false
+        }  
+    ];
 })();
