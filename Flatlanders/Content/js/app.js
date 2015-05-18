cda5340bbc27dca04563b3f33b8cdc0584c85370
Claudia Andrade <andrade8a@gmail.com>
2015-05-17 21:56:42 -0700

    agregando quotes -capitulo 3

--- Flatlanders/Content/js/app.js
+++ Flatlanders/Content/js/app.js
@@ -1,12 +1,12 @@
 ﻿(function () {
     var app = angular.module('store', []);
-    app.controller('StoreController', function () {
+    app.controller('StoreController', function() {
         this.products = gems;
     });
     
-    app.controller('PanelController', function () {
+    app.controller('PanelController', function() {
         this.tab = 1;
-        this.selectedTab = function (setTab) {
+        this.selectTab = function(setTab) {
             this.tab = setTab;
         };
         this.isSelected = function(checkTab){
@@ -14,16 +14,37 @@
         };
     });
 
+    app.controller("ReviewController", function () {
+        this.review = {};
+
+        this.addReview = function (product) {
+            product.reviews.push(this.review);
+            this.review = {};
+        };
+    });
+
     var gems = [
         {
             name: 'Dodecahedron',
             price: 2.95,
-            description: 'this is our best gem because has a lot  of colors and properties thar cure the cancer',
+            description: 'this is our best gem because it has a lot  of colors and properties thar cure the cancer',
             canPurchase: true,
             images: [
                 {
                     full:'/Content/img/gem-02.jpg',
                     thumb:'/Content/img/gem-02.jpg',
+                }
+            ],
+            reviews: [
+                {
+                    stars: 5,
+                    body: "I love this product",
+                    author: "joe@thomas.com"
+                },
+                {
+                    stars: 1,
+                    body: "This product sucks",
+                    author: "tim@hater.com"
                 }
             ]
         },
